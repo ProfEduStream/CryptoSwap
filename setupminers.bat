9674cf981d7225dd471be5c9a2712cc9a8703001
ProfEduStream <111052574+ProfEduStream@users.noreply.github.com>
2022-08-11 13:56:13 +0200

    Add files via upload

--- setupminers.bat
+++ setupminers.bat
@@ -4,11 +4,8 @@ setlocal
 :: defining which model of GPU will mine, where are your mining batch files, and which coins you've set up
 ::============================================================
 set GPU=1070
-set FILE1=C:\Users\Toto-\Desktop\t-rex-0.26.4-win
-set FILE2=C:\Users\Toto-\Desktop\NBMiner_Win
-set FILE3=C:\Users\Toto-\Desktop\1.54
+set FILE=C:\Users\Toto-\Desktop\
 
-set FILE20=C:\Users\Toto-\Desktop\
 set c1=Ethereum(ETH)
 set c2=EthereumClassic(ETC)
 set c3=Ergo(ERG)
@@ -18,16 +15,16 @@ set c6=Ravencoin(RVN)
 ::============================================================
 
 :: searching all words in the column 1 of your %GPU%.csv (this file must have only one cell, the name of the crypto you want to mine, like "Ethereum(ETH)" - cf names on whattomine.com)
-FOR /F "tokens=1" %%i IN (%FILE20%%GPU%.csv) DO (set VARIABLE=%%i)
+FOR /F "tokens=1" %%i IN (%FILE%%GPU%.csv) DO (set VARIABLE=%%i)
 
 :: defining which mines you'll run, at were they're on your computers
 ::============================================================
-if %VARIABLE%==%c1% (start %FILE1%\ETH-ethermine.bat) 
-if %VARIABLE%==%c2% (start %FILE1%\ETC-2miners.bat) 
-if %VARIABLE%==%c3% (start %FILE1%\ERGO-2miners.bat) 
-if %VARIABLE%==%c4% (start %FILE3%\mine_flux.bat) 
-if %VARIABLE%==%c5% (start %FILE1%\CFX-nanopool.bat) 
-if %VARIABLE%==%c6% (start %FILE1%\RVN-2miners.bat)
+if %VARIABLE%==%c1% (start C:\Users\Toto-\Desktop\t-rex-0.26.4-win\ETH-ethermine.bat) 
+if %VARIABLE%==%c2% (start C:\Users\Toto-\Desktop\t-rex-0.26.4-win\ETC-2miners.bat) 
+if %VARIABLE%==%c3% (start C:\Users\Toto-\Desktop\t-rex-0.26.4-win\ERGO-2miners.bat) 
+if %VARIABLE%==%c4% (start C:\Users\Toto-\Desktop\1.54\mine_flux.bat) 
+if %VARIABLE%==%c5% (start C:\Users\Toto-\Desktop\t-rex-0.26.4-winCFX-nanopool.bat) 
+if %VARIABLE%==%c6% (start C:\Users\Toto-\Desktop\t-rex-0.26.4-win\RVN-2miners.bat)
 ::============================================================
 
 endlocal
